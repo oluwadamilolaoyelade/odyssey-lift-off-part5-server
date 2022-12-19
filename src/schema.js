400c6afb3dea10db6124f9257e09d3f99a0996e5
oluwadamilolaoyelade <107127029+oluwadamilolaoyelade@users.noreply.github.com>
2022-12-19 09:25:05 +0100

    .

--- src/schema.js
+++ src/schema.js
@@ -37,9 +37,9 @@ const typeDefs = gql`
     thumbnail: String
     "The track's approximate length to complete, in seconds"
     length: Int @deprecated(reason: "Use durationInSeconds")
-    "The number of modules this track contains"
     "The track's full duration, in seconds"
     durationInSeconds: Int
+    "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
     description: String
@@ -66,7 +66,7 @@ const typeDefs = gql`
     "The module's length in seconds"
     length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's video duration, in seconds"
-    durationInSeconds: Int   
+    durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
